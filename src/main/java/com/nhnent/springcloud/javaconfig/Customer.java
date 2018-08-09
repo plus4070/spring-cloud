97ac3363b3ce2038818779bf80b28c421f87467d
JunyoungOh <plus4070@gmail.com>
2018-08-09 20:49:21 +0900

    - add @ComponentScan(2-8)

---
+++ src/main/java/com/nhnent/springcloud/javaconfig/Customer.java
@@ -0,0 +1,11 @@
+package com.nhnent.springcloud.javaconfig;
+
+public class Customer {
+    private long id;
+    private String email;
+
+    public Customer(long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+}
