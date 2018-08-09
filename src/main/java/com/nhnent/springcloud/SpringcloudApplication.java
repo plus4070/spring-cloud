730708e9424396bde2a42e3a452580f0641c99c1
JunyoungOh <plus4070@gmail.com>
2018-08-09 20:30:47 +0900

    - Add spring cloud application for cats (2-3)

--- src/main/java/com/nhnent/springcloud/SpringcloudApplication.java
+++ src/main/java/com/nhnent/springcloud/SpringcloudApplication.java
@@ -2,6 +2,12 @@ package com.nhnent.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @SpringBootApplication
 public class SpringcloudApplication {
@@ -9,4 +15,35 @@ public class SpringcloudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudApplication.class, args);
 	}
+}
+
+@Entity
+class Cat {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    Cat() {}
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" + "id=" + id + ", name=" + '\'' + '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+@RepositoryRestResource
+interface CatRepository extends JpaRepository<Cat, Long> {
 }
